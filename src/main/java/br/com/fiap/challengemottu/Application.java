7c6b289e012e6caa5817c3f28daebc8c8ee842f1
anacarolcg0 <anacarolcg0@gmail.com>
2025-05-22 12:53:04 -0300

    arrumando...

--- src/main/java/br/com/fiap/challengemottu/Application.java
+++ src/main/java/br/com/fiap/challengemottu/Application.java
@@ -1,10 +1,14 @@
 package br.com.fiap.challengemottu;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "API MOTTU", description = "Challenge API REST", version = "v1"))
 // @EnableCaching // Habilita o suporte a cache do Spring
 public class Application {
 
